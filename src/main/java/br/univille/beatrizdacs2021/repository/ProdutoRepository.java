b0915768466820cc28f4e46a313bccc7c9c1ee1f
beatriz9785 <beatrizschulter@gmail.com>
2021-08-04 01:26:10 +0000

    botao excluir

--- src/main/java/br/univille/beatrizdacs2021/repository/ProdutoRepository.java
+++ src/main/java/br/univille/beatrizdacs2021/repository/ProdutoRepository.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import br.univille.beatrizdacs2021.model.Produto;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-
+    
 
 }
